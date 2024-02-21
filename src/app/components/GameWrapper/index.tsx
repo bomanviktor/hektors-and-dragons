81f325a8c33e81a1a6c98e6c45df9b83e6d9709c
Viktor Boman <viktor.boman@almaco.cc>
2024-02-21 15:17:13 +0200

    Implement ambience

--- src/app/components/GameWrapper/index.tsx
+++ src/app/components/GameWrapper/index.tsx
@@ -16,38 +16,55 @@ const GameWrapper = ({
   music,
   ambience,
   background = "./background.webp",
-  displayGrid = false
+  displayGrid = false,
 }: Params) => {
   const gridLineStyle: React.CSSProperties = {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
     color: "black",
     borderColor: "black",
     opacity: 5,
     borderWidth: 1,
-    zIndex: 10
+    zIndex: 10,
   };
 
   const backgroundStyle: React.CSSProperties = {
     backgroundImage: `url('${background}')`,
-    backgroundSize: '100% 100%',
-    backgroundPosition: 'center',
+    backgroundSize: "100% 100%",
+    backgroundPosition: "center",
   };
 
   return (
-    <main className="relative h-screen w-screen py-10 px-52" style={backgroundStyle}>
-            {displayGrid && (
-        <div style={{ ...gridLineStyle, width: '100%', height: '100%' }}>
+    <main
+      className="relative h-screen w-screen py-10 px-52"
+      style={backgroundStyle}
+    >
+      {displayGrid && (
+        <div style={{ ...gridLineStyle, width: "100%", height: "100%" }}>
           {[...Array(15)].map((_, index) => (
             <React.Fragment key={index}>
-              <div style={{ ...gridLineStyle, left: `${(index + 1) * (100 / 16)}%`, width: '1px', height: '100%' }} />
+              <div
+                style={{
+                  ...gridLineStyle,
+                  left: `${(index + 1) * (100 / 16)}%`,
+                  width: "1px",
+                  height: "100%",
+                }}
+              />
             </React.Fragment>
-              //<div style={{ ...gridLineStyle, top: `${(index + 1) * (100 / 9)}%`, height: '1px', width: '100%' }} />
+            //<div style={{ ...gridLineStyle, top: `${(index + 1) * (100 / 9)}%`, height: '1px', width: '100%' }} />
           ))}
           {[...Array(8)].map((_, index) => (
             <React.Fragment key={index}>
-              <div style={{ ...gridLineStyle, top: `${(index + 1) * (100 / 9)}%`, height: '1px', width: '100%' }} />
+              <div
+                style={{
+                  ...gridLineStyle,
+                  top: `${(index + 1) * (100 / 9)}%`,
+                  height: "1px",
+                  width: "100%",
+                }}
+              />
             </React.Fragment>
           ))}
         </div>
