e724cd5b0f3e979df0b866bd0b93e4480609598f
bomanviktor <boman.viktor@gmail.com>
2024-07-11 17:48:04 +0200

    Update start game state

--- src/app/game/gameState.ts
+++ src/app/game/gameState.ts
@@ -8,18 +8,21 @@ export class GameState {
   difficulty: string;
   players: Player[];
   stage: Stage;
+  night: boolean;
 
   constructor(
     partyName: string,
     difficulty: string,
     players: Character[],
-    chapter = "goetia",
-    stage = new Stage("goetia", 2, 0, 0),
+    chapter = "elysia",
+    stage = new Stage("elysia", -1, 3, 0),
+    night = false
   ) {
     this.chapter = chapter;
     this.partyName = partyName;
     this.difficulty = difficulty;
     this.players = Player.getPlayers(players);
     this.stage = stage;
+    this.night = night;
   }
 }
